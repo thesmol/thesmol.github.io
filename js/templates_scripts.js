002c2ae6d67cfeb9e1305103abedfc070964b396
Vanya <ranzar2000@gmail.com>
2023-03-26 18:15:53 +0300

    Add hightlight to the button of current page

---
+++ js/templates_scripts.js
@@ -0,0 +1,11 @@
+function highlightCurrentPage() {
+    var url = window.location;
+    $('ul.nav a[href="'+ url +'"]').parent().addClass('active');
+    $('ul.nav a').filter(function() {
+       return this.href == url;
+    }).parent().addClass('active');
+  }
+  
+$(document).ready(function () {
+highlightCurrentPage();
+});
