9a295a3d4c971e36dbf3fb17f07d962bc34ea10f
Vanya <ranzar2000@gmail.com>
2023-03-31 11:16:42 +0300

    fix prev fixes...

--- js/templates_scripts.js
+++ js/templates_scripts.js
@@ -19,54 +19,48 @@
 //   });
 // });
 
-$(document).ready(function() {
-  // получаем ссылки на элементы меню
-  var menuLinks = document.querySelectorAll('.nav-item');
-
-  // обрабатываем клик по каждой ссылке
-  menuLinks.forEach(function(link) {
-    link.addEventListener('click', function(e) {
-      e.preventDefault(); // отменяем стандартное действие ссылки
-
-      // отправляем AJAX-запрос на сервер
-      var xhr = new XMLHttpRequest();
-      xhr.open('GET', link.href);
-      xhr.onload = function() {
-        if (xhr.status === 200) {
-          // если запрос успешен, заменяем содержимое страницы на полученный HTML-код
-          var content = document.querySelector('body');
-          content.innerHTML = xhr.responseText;
-
-          // подсвечиваем текущую страницу
-          highlightCurrentPage();
-
-          // меняем URL страницы
-          history.pushState(null, null, link.href);
-        }
-      };
-      xhr.send();
-    });
+$(document).ready(function() { 
+    // получаем ссылки на элементы меню 
+  var menuLinks = document.querySelectorAll('.nav-item'); 
+   
+  // обрабатываем клик по каждой ссылке 
+  menuLinks.forEach(function(link) { 
+    link.addEventListener('click', function(e) { 
+      e.preventDefault(); // отменяем стандартное действие ссылки 
+   
+      // отправляем AJAX-запрос на сервер 
+      var xhr = new XMLHttpRequest(); 
+      xhr.open('GET', link.href); 
+      xhr.onload = function() { 
+        if (xhr.status === 200) { 
+          // если запрос успешен, заменяем содержимое страницы на полученный HTML-код 
+          var content = document.querySelector('body'); 
+          content.innerHTML = xhr.responseText; 
+          // меняем URL страницы 
+          history.pushState(null, null, link.href); 
+          highlightCurrentPage(); 
+        } 
+      }; 
+      xhr.send(); 
+    }); 
+  }); 
+   
+  // обрабатываем событие изменения URL страницы 
+  window.addEventListener('popstate', function(e) { 
+    // отправляем AJAX-запрос на сервер с новым URL 
+    var xhr = new XMLHttpRequest(); 
+    xhr.open('GET', location.href); 
+    xhr.onload = function() { 
+      if (xhr.status === 200) { 
+        // если запрос успешен, заменяем содержимое страницы на полученный HTML-код 
+        var content = document.querySelector('body'); 
+        content.innerHTML = xhr.responseText; 
+        highlightCurrentPage(); 
+      } 
+    }; 
+    xhr.send(); 
+  }); 
   });
-
-  // обрабатываем событие изменения URL страницы
-  window.addEventListener('popstate', function(e) {
-    // отправляем AJAX-запрос на сервер с новым URL
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', location.href);
-    xhr.onload = function() {
-      if (xhr.status === 200) {
-        // если запрос успешен, заменяем содержимое страницы на полученный HTML-код
-        var content = document.querySelector('body');
-        content.innerHTML = xhr.responseText;
-
-        // подсвечиваем текущую страницу
-        highlightCurrentPage();
-      }
-    };
-    xhr.send();
-  });
-});
-
 
 function highlightCurrentPage() {
   var url = window.location;
