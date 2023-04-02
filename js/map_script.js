bb91448dabdb3da39e3fa1b730d98e9945eb949b
Vanya <ranzar2000@gmail.com>
2023-04-02 17:03:27 +0300

    format map script

--- js/map_script.js
+++ js/map_script.js
@@ -2,25 +2,25 @@
 ymaps.ready(init);
 
 function init() { 
-var map; 
-var placemark; 
- map = new ymaps.Map("map", { 
-  center: [56.749905, 37.141429], 
-  zoom: 14, 
- }); 
- placemark = new ymaps.Placemark([56.749905, 37.141429], {}, { 
-  preset: "islands#redDotIcon", 
-  draggable: true 
- }); 
- 
- map.geoObjects.add(placemark); 
- 
- map.events.add("boundschange", function() { 
-  showLoader(); 
- }); 
-  ymaps.ready(function() {
-  hideLoader();
-  });
+    var map; 
+    var placemark; 
+    map = new ymaps.Map("map", { 
+        center: [56.749905, 37.141429], 
+        zoom: 14, 
+    }); 
+    placemark = new ymaps.Placemark([56.749905, 37.141429], {}, { 
+        preset: "islands#redDotIcon", 
+        draggable: true 
+    }); 
+    
+    map.geoObjects.add(placemark); 
+    
+    map.events.add("boundschange", function() { 
+        showLoader(); 
+    }); 
+    ymaps.ready(function() {
+        hideLoader();
+    });
 }
 
 function showLoader() {
