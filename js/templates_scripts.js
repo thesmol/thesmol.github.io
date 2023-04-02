325d24dc5527541ec48bb25cd1b2239b55195f75
Vanya <ranzar2000@gmail.com>
2023-04-02 13:05:57 +0300

    Revert "rewrite function for removing loadings"
    This reverts commit e64362e2655b8ace0f8cadeb6b5d67a759b36981.

--- js/templates_scripts.js
+++ js/templates_scripts.js
@@ -19,73 +19,48 @@
 //   });
 // });
 
-// $(document).ready(function() { 
-//     // получаем ссылки на элементы меню 
-//   var menuLinks = document.querySelectorAll('.nav-link'); 
+$(document).ready(function() { 
+    // получаем ссылки на элементы меню 
+  var menuLinks = document.querySelectorAll('.nav-link'); 
    
-//   // обрабатываем клик по каждой ссылке 
-//   menuLinks.forEach(function(link) { 
-//     link.addEventListener('click', function(e) { 
-//       e.preventDefault(); // отменяем стандартное действие ссылки 
+  // обрабатываем клик по каждой ссылке 
+  menuLinks.forEach(function(link) { 
+    link.addEventListener('click', function(e) { 
+      e.preventDefault(); // отменяем стандартное действие ссылки 
    
-//       // отправляем AJAX-запрос на сервер 
-//       var xhr = new XMLHttpRequest(); 
-//       xhr.open('GET', link.href); 
-//       xhr.onload = function() { 
-//         if (xhr.status === 200) { 
-//           // если запрос успешен, заменяем содержимое страницы на полученный HTML-код 
-//           var content = document.querySelector('html'); 
-//           content.innerHTML = xhr.responseText; 
-//           // меняем URL страницы 
-//           history.pushState(null, null, link.href); 
-//           highlightCurrentPage(); 
-//         } 
-//       }; 
-//       xhr.send(); 
-//     }); 
-//   }); 
+      // отправляем AJAX-запрос на сервер 
+      var xhr = new XMLHttpRequest(); 
+      xhr.open('GET', link.href); 
+      xhr.onload = function() { 
+        if (xhr.status === 200) { 
+          // если запрос успешен, заменяем содержимое страницы на полученный HTML-код 
+          var content = document.querySelector('html'); 
+          content.innerHTML = xhr.responseText; 
+          // меняем URL страницы 
+          history.pushState(null, null, link.href); 
+          highlightCurrentPage(); 
+        } 
+      }; 
+      xhr.send(); 
+    }); 
+  }); 
    
-//   // обрабатываем событие изменения URL страницы 
-//   window.addEventListener('popstate', function(e) { 
-//     // отправляем AJAX-запрос на сервер с новым URL 
-//     var xhr = new XMLHttpRequest(); 
-//     xhr.open('GET', location.href); 
-//     xhr.onload = function() { 
-//       if (xhr.status === 200) { 
-//         // если запрос успешен, заменяем содержимое страницы на полученный HTML-код 
-//         var content = document.querySelector('html'); 
-//         content.innerHTML = xhr.responseText; 
-//         highlightCurrentPage(); 
-//       } 
-//     }; 
-//     xhr.send(); 
-//   }); 
-//   });
-
-$(document).ready(function() {
-  // Обработчик клика на ссылке
-  $('.nav-item').on('click', function(event) {
-    event.preventDefault();
-    highlightCurrentPage()
-
-    // Получаем URL и заголовок страницы, на которую переходим
-    var url = $(this).attr('href');
-    var title = $(this).text();
-
-    // Меняем URL и заголовок страницы без перезагрузки
-    history.pushState(null, title, url);
-    document.title = title;
-
-    // Загружаем содержимое страницы с помощью AJAX-запроса
-    $.ajax({
-      url: url,
-      success: function(data) {
-        // Обновляем содержимое страницы
-        $('#content').html(data);
-      }
-    });
+  // обрабатываем событие изменения URL страницы 
+  window.addEventListener('popstate', function(e) { 
+    // отправляем AJAX-запрос на сервер с новым URL 
+    var xhr = new XMLHttpRequest(); 
+    xhr.open('GET', location.href); 
+    xhr.onload = function() { 
+      if (xhr.status === 200) { 
+        // если запрос успешен, заменяем содержимое страницы на полученный HTML-код 
+        var content = document.querySelector('html'); 
+        content.innerHTML = xhr.responseText; 
+        highlightCurrentPage(); 
+      } 
+    }; 
+    xhr.send(); 
+  }); 
   });
-});
 
 function highlightCurrentPage() {
   var url = window.location;
