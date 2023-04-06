86c048f46fc469669002133b6ce6fb6351b0fd8e
Vanya <ranzar2000@gmail.com>
2023-04-06 19:34:04 +0300

    rewrite templatres_stripts using ajax syntax

--- js/templates_scripts.js
+++ js/templates_scripts.js
@@ -1,31 +1,4 @@
 // general
-
-// $(document).ready(function() {
-//   highlightCurrentPage()
-//   // Обработчик клика на ссылке
-//   $('.nav-link').on('click', function(event) {
-//     event.preventDefault();
-
-//     // Получаем URL и заголовок страницы, на которую переходим
-//     var url = $(this).attr('href');
-//     var title = $(this).text();
-
-//     // Меняем URL и заголовок страницы без перезагрузки
-//     history.pushState(null, title, url);
-//     document.title = title;
-
-//     // Загружаем содержимое страницы с помощью AJAX-запроса
-//     $.ajax({
-//       url: url,
-//       success: function(data) {
-//         // Обновляем содержимое страницы
-//         $('html').html(data);
-//       }
-//     });
-//     highlightCurrentPage()
-//   });
-// });
-
 $(document).ready(function() { 
   // устанавливаем начальное время для таймера
   let startTime = localStorage.getItem('startTime');
@@ -38,48 +11,46 @@ $(document).ready(function() {
   // подсвеиваем элемент меню навигации, соотвествующий текущей странице
   highlightCurrentPage();
     // получаем ссылки на элементы меню 
-  var menuLinks = document.querySelectorAll('.nav-link'); 
-   
+  var menuLinks = document.querySelectorAll('.nav-link');
+
   // обрабатываем клик по каждой ссылке 
-  menuLinks.forEach(function(link) { 
-    link.addEventListener('click', function(e) { 
-      e.preventDefault(); // отменяем стандартное действие ссылки 
-   
-      // отправляем AJAX-запрос на сервер 
-      var xhr = new XMLHttpRequest(); 
-      xhr.open('GET', link.href); 
-      xhr.onload = function() { 
-        if (xhr.status === 200) { 
+  menuLinks.forEach(function(link) {
+    link.addEventListener('click', function(e) {
+      e.preventDefault(); // отменяем стандартное действие ссылки
+  
+      // отправляем AJAX-запрос на сервер
+      $.ajax({
+        url: link.href,
+        method: 'GET',
+        success: function(data) {
           // если запрос успешен, заменяем содержимое страницы на полученный HTML-код 
-          var content = document.querySelector('html'); 
-          content.innerHTML = xhr.responseText; 
+          var content = $('html');
+          content.html(data);
           // меняем URL страницы 
-          history.pushState(null, null, link.href); 
-
-          highlightCurrentPage(); 
-        } 
-      }; 
-      xhr.send(); 
-    }); 
-  }); 
-   
-  // обрабатываем событие изменения URL страницы 
-  window.addEventListener('popstate', function(e) { 
-    // отправляем AJAX-запрос на сервер с новым URL 
-    var xhr = new XMLHttpRequest(); 
-    xhr.open('GET', location.href); 
-    xhr.onload = function() { 
-      if (xhr.status === 200) { 
-        // если запрос успешен, заменяем содержимое страницы на полученный HTML-код 
-        var content = document.querySelector('html'); 
-        content.innerHTML = xhr.responseText; 
-
-        highlightCurrentPage();
-      } 
-    }; 
-    xhr.send(); 
-  }); 
+          history.pushState(null, null, link.href);
+  
+          highlightCurrentPage();
+        }
+      });
+    });
   });
+  
+  // обрабатываем событие изменения URL страницы 
+  window.addEventListener('popstate', function(e) {
+    // отправляем AJAX-запрос на сервер с новым URL 
+    $.ajax({
+      url: location.href,
+      method: 'GET',
+      success: function(data) {
+        // если запрос успешен, заменяем содержимое страницы на полученный HTML-код 
+        var content = $('html');
+        content.html(data);
+  
+        highlightCurrentPage();
+      }
+    });
+  });
+});
 
 function highlightCurrentPage() {
   var url = window.location;
@@ -87,24 +58,4 @@ function highlightCurrentPage() {
   $('ul.nav a').filter(function() {
       return this.href == url;
   }).parent().addClass('active');
-  console.log("подсветили")
 }
-
-// function loadScripts() {  
-//   // получаем все скрипты на странице  
-//   var scripts = document.querySelectorAll('.page-script');   
-//   // создаем новый скрипт для каждого существующего на странице  
-//   for (var i = 0; i < scripts.length; i++) {  
-//     var script = document.createElement('script');  
-//     script.type = scripts[i].type;  
-//     script.src = scripts[i].src; 
-//     console.log("добавили"); 
-//     // добавляем новый скрипт на страницу и удаляем старый  
-//     scripts[i].parentNode.insertBefore(script, scripts[i]);  
-//     scripts[i].parentNode.removeChild(scripts[i]);  
-//   }  
-// }
-  
-// $(document).ready(function () {
-// highlightCurrentPage();
-// });
