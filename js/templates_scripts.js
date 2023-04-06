996d93a4a26f98a2c162b82bc342671291ea3763
Vanya <ranzar2000@gmail.com>
2023-04-06 19:31:27 +0300

    Revert "write script for timer using local storage and cookies"
    This reverts commit 4653c2c8d9cc5ee70721d7c98c5e09616e7e8032.

--- js/templates_scripts.js
+++ js/templates_scripts.js
@@ -28,20 +28,13 @@
 
 $(document).ready(function() { 
   // устанавливаем начальное время для таймера
-  // let startTime = localStorage.getItem('startTime');
-  //   if (!startTime) {
-  //   // если время еще не сохранено, сохраняем текущее время
-  //   startTime = Date.now();
-  //   localStorage.setItem('startTime', startTime);
-  //   };
-
-  let startTime = getCookie('startTime');
+  let startTime = localStorage.getItem('startTime');
     if (!startTime) {
-        // если время еще не сохранено, сохраняем текущее время
-        startTime = Date.now();
-        setCookie('startTime', startTime);
-    }
-    
+    // если время еще не сохранено, сохраняем текущее время
+    startTime = Date.now();
+    localStorage.setItem('startTime', startTime);
+    };
+
   // подсвеиваем элемент меню навигации, соотвествующий текущей странице
   highlightCurrentPage();
     // получаем ссылки на элементы меню 
--- js/timer_script.js
+++ js/timer_script.js
@@ -1,18 +1,10 @@
 $(document).ready(function(){
-    // // получаем время из локального хранилища, если оно там есть
-    // let startTime = localStorage.getItem('startTime');
-    // if (!startTime) {
-    // // если время еще не сохранено, сохраняем текущее время
-    // startTime = Date.now();
-    // localStorage.setItem('startTime', startTime);
-    // }
-
-    // получаем время из куки, если оно там есть
-    let startTime = getCookie('startTime');
+    // получаем время из локального хранилища, если оно там есть
+    let startTime = localStorage.getItem('startTime');
     if (!startTime) {
-        // если время еще не сохранено, сохраняем текущее время
-        startTime = Date.now();
-        setCookie('startTime', startTime);
+    // если время еще не сохранено, сохраняем текущее время
+    startTime = Date.now();
+    localStorage.setItem('startTime', startTime);
     }
 
     // запускаем таймер
@@ -28,25 +20,32 @@ $(document).ready(function(){
     document.getElementById('timer').innerText = timeString;
     }
     
-});
-// функция для установки куки
-function setCookie(name, value, days = 30) {
-    const date = new Date();
-    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-    const expires = 'expires=' + date.toUTCString();
-    document.cookie = name + '=' + value + ';' + expires + ';path=/';
-}
-
-// функция для получения куки
-function getCookie(name) {
-    const cookies = document.cookie.split(';');
-    for (let i = 0; i < cookies.length; i++) {
-        const cookie = cookiesi.trim();
-        if (cookie.startsWith(name + '=')) {
-        return cookie.substring(name.length + 1, cookie.length);
-        }
+    window.addEventListener('beforeunload', function (e) {
+        // Установить cookie, чтобы указать, что пользователь покидает страницу
+        document.cookie = 'leaving=true; expires=' + new Date(new Date().getTime() + 24 * 60 * 60 * 1000).toUTCString(); // Устанавливаем cookie на истечение 24 часов
+      
+        // Удалить файл cookie, указывающий на то, что пользователь перезагружает страницу, если он существует
+        document.cookie = 'reloading=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+      });
+      
+    window.addEventListener('load', function (e) {
+    // Проверка, установлен ли файл cookie, чтобы отличить перезагрузку страницы от ухода пользователя со страницы
+    if (document.cookie.indexOf('leaving=') !== -1) {
+        // пользователь покидает страницу:
+        localStorage.removeItem('startTime');
+        // удаялем cookie, чтобы он не мешал загрузке страницы в будущем
+        document.cookie = 'leaving=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+    } else if (document.cookie.indexOf('reloading=') !== -1) {
+        // удаялем cookie, чтобы он не мешал загрузке страницы в будущем
+        document.cookie = 'reloading=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+    } else {
+        // установка cookie, чтобы указать, что пользователь перезагружает страницу
+        document.cookie = 'reloading=true; expires=' + new Date(new Date().getTime() + 24 * 60 * 60 * 1000).toUTCString(); // Устанавливаем срок действия cookie через 24 часа
     }
-    return null;
-}
+    });
+});
+
+
+
 
 
