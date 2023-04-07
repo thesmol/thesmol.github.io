7f2bbe0738352e5668959c258fbd2b15f0fe9cd2
Vanya <ranzar2000@gmail.com>
2023-04-07 09:38:33 +0300

    remove not propertly  working part

--- js/templates_scripts.js
+++ js/templates_scripts.js
@@ -4,45 +4,19 @@ $(document).ready(function() {
   let timeSpent = 0;
 
   // получаем значение куки "timeSpent"
-  let cookieValue = document.cookie.replace(/(?:(?:^|.*;\s*)timeSpent\s*\=\s*([^;]*).*$)|^.*$/, "$1");
+  let cookieValueTimer = document.cookie.replace(/(?:(?:^|.*;\s*)timeSpent\s*\=\s*([^;]*).*$)|^.*$/, "$1");
 
   // если куки есть, берем из них значение времени
-  if (cookieValue) {
-      timeSpent = parseInt(cookieValue);
+  if (cookieValueTimer) {
+      timeSpent = parseInt(cookieValueTimer);
       startTime = Date.now() - timeSpent;
   }
 
   // Зарегистрируем событие `beforeunload`.
   window.addEventListener('beforeunload', function(event) {
-    // Сохраняем текущую временную метку в cookie
-    document.cookie = 'pageUnloadTime=' + Date.now();
+    let timeSpent = Date.now() - startTime;
+    document.cookie = "timeSpent=" + timeSpent;
   });
-
-  // Зарегистрируем событие `load`.
-  window.addEventListener('load', function(event) {
-    // Считываем куки, установленные событием beforeunload
-    var cookies = document.cookie.split(';');
-    var pageUnloadTime;
-    for (var i = 0; i < cookies.length; i++) {
-      var cookie = cookies[i].trim();
-      if (cookie.indexOf('pageUnloadTime=') === 0) {
-        pageUnloadTime = parseInt(cookie.substring(15));
-      }
-    }
-    // Если cookie установлен, страница была завершена, в противном случае она была обновлена
-    if (pageUnloadTime) {
-      document.cookie = "timeSpent=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-      console.log('Покинуть страницу');
-    } else {
-      let timeSpent = Date.now() - startTime;
-      document.cookie = "timeSpent=" + timeSpent + "; SameSite=strict";
-      console.log('Обновление страницы');
-    }
-
-    // Очистить cookie
-    document.cookie = 'pageUnloadTime=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;';
-  });
-
   // подсвеиваем элемент меню навигации, соотвествующий текущей странице
   highlightCurrentPage();
   //   // получаем ссылки на элементы меню 
