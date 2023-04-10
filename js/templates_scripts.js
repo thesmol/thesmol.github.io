fe1b903bd8e330bf994777c31a701a6f62ce3b62
Vanya <ranzar2000@gmail.com>
2023-04-10 23:12:05 +0300

    fix return for formatTime func

--- js/templates_scripts.js
+++ js/templates_scripts.js
@@ -99,6 +99,6 @@ function formatTime(time) {
   let hours = Math.floor(time / (1000 * 60 * 60));
   let minutes = Math.floor((time % (1000 * 60 * 60)) / (1000 * 60));
   let seconds = Math.floor((time % (1000 * 60)) / 1000);
-  
-  return `${hours}:${minutes}:${seconds}`;
+  let timer = hours.toString().padStart(2, "0") + ":" + minutes.toString().padStart(2, "0") + ":" + seconds.toString().padStart(2, "0");
+  return timer;
 }
