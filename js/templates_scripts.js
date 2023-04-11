5aff0cc85f9a26c98ba58cb54d6ae63afad63b90
Vanya <ranzar2000@gmail.com>
2023-04-11 22:15:52 +0300

    Revert "Merge pull request #63 from thesmol/hot_fixes"
    This reverts commit 4106d66e7c75e3b923453d6c40a02dd3e76ba2d2, reversing
    changes made to 980abd6d38bbd68b94ff7a093e927d86b9f211e2.

--- js/templates_scripts.js
+++ js/templates_scripts.js
@@ -41,7 +41,7 @@ $(document).ready(function () {
      if (sessionStorage.getItem(cacheKey)) {
       // удаляем подсветку предыдущего элемента навигационного меню
       removeActiveClass();
-      var mainContent = $(sessionStorage.getItem(cacheKey)).html().find('#mainContent');
+      var mainContent = $(sessionStorage.getItem(cacheKey)).find('#mainContent').html();
       // Если данные есть в кэше используем их
       $('#mainContent').html(mainContent);
 
@@ -75,9 +75,9 @@ $(document).ready(function () {
             removeActiveClass();
 
             // Находим элемент "main" в полученном HTML-коде
-            var mainContent = $(data).html().find('#mainContent');
+            var mainContent = $(data).find('#mainContent').html();
             // Заменяем содержимое элемента "main" на странице
-            $('#mainContent').html(mainContent);
+            $('#mainContent').html(data);
             timerElem = document.getElementById('timer');
             mapElem = document.getElementById('map');
 
@@ -113,7 +113,7 @@ $(document).ready(function () {
       // удаляем подсветку предыдущего элемента навигационного меню
       removeActiveClass();
       // Если данные есть в кэше используем их
-      var mainContent = $(sessionStorage.getItem(cacheKey)).html().find('#mainContent');
+      var mainContent = $(sessionStorage.getItem(cacheKey)).find('#mainContent').html();
       $('#mainContent').html(mainContent);
 
       timerElem = document.getElementById('timer');
@@ -145,7 +145,7 @@ $(document).ready(function () {
             removeActiveClass();
 
             // Находим элемент "main" в полученном HTML-коде
-            var mainContent = $(data).html().find('#mainContent');
+            var mainContent = $(data).find('#mainContent').html();
             // Заменяем содержимое элемента "main" на странице
             $('#mainContent').html(mainContent);
             timerElem = document.getElementById('timer');
